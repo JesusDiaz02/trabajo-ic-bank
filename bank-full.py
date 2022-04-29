882cacaed8afc3a552ac465cb9a9673d812fe500
jesus diaz <jesusdiazg9@gmail.com>
2022-04-28 23:06:48 -0500

    tercer commit

--- bank-full.py
+++ bank-full.py
@@ -9,3 +9,21 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import neighbors
 
 simplefilter(action='ignore', category=FutureWarning)
+
+url ='bank-full.csv'
+data = pd.read_csv(url)
+data.drop(['balance', 'duration', 'pdays'],axis=1, inplace=True)
+
+data['job'].replace(['blue-collar','management','technician','admin.',
+'services','retired','self-employed','entrepreneur','unemployed',
+'housemaid','student','unknown'], [0,1,2,3,4,5,6,7,8,9,10,11], inplace=True)
+
+data['marital'].replace(['married','single','divorced'], [0, 1, 2], inplace=True)
+
+data['education'].replace(['secondary','tertiary', 'primary','unknown'], [0, 1, 2, 3], inplace=True)
+
+data.default.replace(['no', 'yes'], [0, 1], inplace=True)
+
+data.housing.replace(['yes', 'no'], [0, 1], inplace=True)
+
+data.loan.replace(['no', 'yes'], [0, 1], inplace=True)
